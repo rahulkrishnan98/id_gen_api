a9b97bc455fcc457b33871b93d2b12f3174d648b
rahulkrishnan98 <mrahul.krishnan@gmail.com>
2020-05-13 17:15:11 +0530

    get and delete

--- api/routes/orders.js
+++ api/routes/orders.js
@@ -7,8 +7,29 @@ const Idcard = require("../models/idcard");
 
 //Handling incoming requests
 router.get('/', (req, res, next) => {
-
+    Order.find().select("_id organizationName").exec().then(docs => {
+        const response = {
+            order_count: docs.length,
+            orders: docs.map(doc => {
+                return {
+                    _id: doc._id,
+                    organizationName: doc.organizationName,
+                    request: {
+                        type: 'GET',
+                        url: 'http://localhost:3000/orders/' + doc._id
+                    }
+                };
+            })
+        };
+        res.status(200).json(response);
+    }).catch(err => {
+        console.log(err);
+        res.status(500).json({
+            error: err
+        });
+    });
 });
+
 
 router.post('/', (req, res, next) => {
     const promisedMap = new Promise(function (resolve, reject) {
@@ -61,4 +82,26 @@ router.post('/', (req, res, next) => {
     })
 
 });
+
+router.delete('/:orderId', (req, res, next) => {
+    const id = req.params.orderId;
+    Idcard.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json({
+                message: 'ID deleted from Database',
+                request: {
+                    type: 'POST',
+                    url: 'http://localhost:3000/orders',
+                    body: { organizationName: 'String', orderItem: "idCardArray" }
+                }
+            });
+        }).catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            });
+        });
+})
+
 module.exports = router;
